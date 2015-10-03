152aa89958d631cb67e441dcced730f5dc295f26
Steven Marr <stevenmarr@me.com>
2015-10-03 13:24:06 -0600

    Initial Commit

---
+++ tvOS-client-server-tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tvOS-client-server-tutorial
+//
+//  Created by Steven Marr on 10/3/15.
+//  Copyright © 2015 Steven Marr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
